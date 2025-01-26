1d521d29275f743bdddf4b4bca9ad5924380a509
arimu-trumpet <104705709+arimu-trumpet@users.noreply.github.com>
2025-01-26 12:05:59 +0000

    FutureGateWay

--- Assets/Scripts/EndWorldAnimator.cs
+++ Assets/Scripts/EndWorldAnimator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationController : MonoBehaviour
+public class EndWorldAnimator : MonoBehaviour
 {
     [SerializeField] private Animator _meteoriteImpact;
 
